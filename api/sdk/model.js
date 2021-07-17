f66439ddfce050533aa2ed173a0cb3f24503a604
wawanpratama10 <84949517+wawanpratama10@users.noreply.github.com>
2021-07-17 16:24:47 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -25,7 +25,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/wawanpratama10/uas_wawan/main/public/cls_model/model.json';
+        const path = 'https://raw.githubusercontent.com/wawanpratama10/uas_wawan/main/public/ex_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
