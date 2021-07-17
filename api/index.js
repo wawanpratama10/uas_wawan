2f3549859f6f127cb964b947b19af6d1c49d8110
wawanpratama10 <84949517+wawanpratama10@users.noreply.github.com>
2021-07-17 11:54:28 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1779091825:AAHg20dEekOkD0WwCHcl9vBvdHmpa89SyRU'
+const token = '1896178928:AAES5KkwolLLvefHvvoOnkcqiipDPOZtTc4'
 const bot = new TelegramBot(token, {polling: true});
 
 
