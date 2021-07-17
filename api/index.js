cafce05efb2ffbd1d1be61faa7da53a8495cd621
wawanpratama10 <84949517+wawanpratama10@users.noreply.github.com>
2021-07-17 12:25:41 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1896178928:AAES5KkwolLLvefHvvoOnkcqiipDPOZtTc4'
+const token = '1918739445:AAGwfICp5QCLQ4oefWhvvmoPK8VQs0cz1Ks'
 const bot = new TelegramBot(token, {polling: true});
 
 
@@ -16,17 +16,15 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click  /predict to know about x1 x2 and x3`
+        click /predict`
     );   
 });
 
-
-
-state = 0;
+state = 0
 bot.onText(/\/predict/, (msg) => { 
     bot.sendMessage(
         msg.chat.id,
-        `input nilai x1|x2|x3 example 9|6|3`
+        `input nilai x1|x2|x3 contohnya 5|4|3 (sesuai dengan nilai pada dataset)`
     );   
     state = 1;
 });
@@ -34,9 +32,8 @@ bot.onText(/\/predict/, (msg) => {
 bot.on('message', (msg) => {
     if(state == 1){
         s = msg.text.split("|");
-        x1 = s[0]
-        x2 = s[1]
-        x3 = s[2]
+        x1 = s [0]
+        y1 = s [1]
         model.predict(
             [
                 parseFloat(s[0]), // string to float
@@ -46,25 +43,25 @@ bot.on('message', (msg) => {
         ).then((jres)=>{
             bot.sendMessage(
                 msg.chat.id,
-                `nilai x1 yang diprediksi adalah ${jres[0]} `
-            );   
+                `nilai y1 yang diprediksi adalah ${jres [0]} `
+                
+            ); 
             bot.sendMessage(
                 msg.chat.id,
-                `nilai x2 yang diprediksi adalah ${jres[1]} `
-            );   
-            bot.sendMessage(
+                `nilai y2 yang diprediksi adalah ${jres [1]} `
+            );
+            
+             bot.sendMessage(
                 msg.chat.id,
-                `nilai x3 yang diprediksi adalah ${jres[2]} `
-         );   
-})
+                `nilai y3 yang diprediksi adalah ${jres [2]} `
+            );
+        })
     }else{
         state = 0
     }
 })
-
-
 // routers
-r.get('/prediction/:x2/:x2/:x3', function(req, res, next) {    
+r.get('/prediction/:x1/:x2/:x3', function(req, res, next) {    
     model.predict(
         [
             parseFloat(req.params.x1), // string to float
